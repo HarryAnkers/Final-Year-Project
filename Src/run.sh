cbde2b5d9093fcce7c9eec801ea1cb06f5ceac87
Harrison Ankers <h-ankers@outlook.com>
2020-04-24 13:47:41 +0100

    added time per file

--- Src/run.sh
+++ Src/run.sh
@@ -1,3 +1,6 @@
+echo "=== BEGINING JULIA FUZZER ==="
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 JULIA=/Applications/Julia-1.3.app/Contents/Resources/julia/bin/julia
 # Declares how many processes are wanted
 let pro_N=0
@@ -32,13 +35,13 @@ while [ -n "$1" ]; do # while loop starts
 done
 
 # Removes all old test files
-if [[ ! -d "./test_files/Process_$pro_N" ]]
+if [[ ! -d "$DIR/test_files/Process_$pro_N" ]]
 then
-    mkdir "./test_files/Process_$pro_N"
-    mkdir "./test_files/Process_$pro_N/log_files"
+    mkdir "$DIR/test_files/Process_$pro_N"
+    mkdir "$DIR/test_files/Process_$pro_N/log_files"
 fi
 
-echo "process $i started"
+let start=$(gdate +%s)
 while :
 do
     let bug=1
@@ -47,15 +50,15 @@ do
     rm -rfv ./test_files/log_files/*.txt 2>/dev/null
 
     # Creates the files
-    $JULIA "topLevel.jl" "./test_files/Process_$pro_N/"
+    $JULIA "$DIR/topLevel.jl" "$DIR/test_files/Process_$pro_N/"
     # Ran at each opt. level results are stored in variables e.g. o0_return
-    $JULIA "--optimize=0" "./test_files/Process_$pro_N/File.jl" 1 0 2>/dev/null
+    $JULIA "--optimize=0" "$DIR/test_files/Process_$pro_N/File.jl" 1 0 2>/dev/null
     let o0_return=$?
-    $JULIA "--optimize=1" "./test_files/Process_$pro_N/FILE.jl" 1 1 2>/dev/null
+    $JULIA "--optimize=1" "$DIR/test_files/Process_$pro_N/FILE.jl" 1 1 2>/dev/null
     let o1_return=$?
-    $JULIA "--optimize=2" "./test_files/Process_$pro_N/FILE.jl" 1 2 2>/dev/null
+    $JULIA "--optimize=2" "$DIR/test_files/Process_$pro_N/FILE.jl" 1 2 2>/dev/null
     let o2_return=$?
-    $JULIA "--optimize=3" "./test_files/Process_$pro_N/FILE.jl" 1 3 2>/dev/null
+    $JULIA "--optimize=3" "$DIR/test_files/Process_$pro_N/FILE.jl" 1 3 2>/dev/null
     let o3_return=$?
 
     # Checks if file threw an error and if it did inc. the count
@@ -71,11 +74,11 @@ do
     elif [ $o2_return -ne $o3_return ]; then
         echo "error diff in File $i o2 / o3 ($o2_return-$o3_return)"
     elif [ $o0_return -eq 0 ]; then
-        if ! cmp -s ./test_files/Process_$pro_N/log_files/log0.txt ./test_files/Process_$pro_N/log_files/log1.txt; then
+        if ! cmp -s "$DIR/test_files/Process_$pro_N/log_files/log0.txt" "$DIR/test_files/Process_$pro_N/log_files/log1.txt"; then
             echo "log diff in File $i o0 / o1"
-        elif ! cmp -s ./test_files/Process_$pro_N/log_files/log1.txt ./test_files/Process_$pro_N/log_files/log2.txt; then
+        elif ! cmp -s "$DIR/test_files/Process_$pro_N/log_files/log1.txt" "$DIR/test_files/Process_$pro_N/log_files/log2.txt"; then
             echo "log diff in File $i o1 / o2"
-        elif ! cmp -s ./test_files/Process_$pro_N/log_files/log2.txt ./test_files/Process_$pro_N/log_files/log3.txt; then
+        elif ! cmp -s "$DIR/test_files/Process_$pro_N/log_files/log2.txt" "$DIR/test_files/Process_$pro_N/log_files/log3.txt"; then
             echo "log diff in File $i o2 / o3"
         else
             bug=0
@@ -86,14 +89,16 @@ do
     
     if [ $bug -eq 1 ]; then
         echo "Bug found!"
-        cp ./test_files/Process_$pro_N/FILE.jl ./test_files/bug_files/test_$(date +%S:%M:%H-%F).jl
+        cp "$DIR/test_files/Process_$pro_N/FILE.jl" "$DIR/test_files/bug_files/test_$(date +%S:%M:%H-%F).jl"
     elif [ $error -eq 1 ]; then
         if [ $noPrint -eq 1 ]; then
-            cp ./test_files/Process_$pro_N/FILE.jl ./test_files/error_files/test_$(date +%S:%M:%H-%F).jl
+            cp "$DIR/test_files/Process_$pro_N/FILE.jl" "$DIR/test_files/error_files/test_$(date +%S:%M:%H-%F).jl"
         fi
     fi
+
+    let end=$(gdate +%s)
     if [ $noPrint -ne 1 ]; then
         file_counter=$((file_counter+1))
-        echo -en "done file number - $file_counter \r"
+        echo -en "done file number - $file_counter [$(bc <<< "scale=2 ; ($end - $start) / $file_counter") per file]\r"
     fi
 done
