1e0f3f1d69ec757e6173e0fa65e776eda74a3993
Harrison Ankers <h-ankers@outlook.com>
2020-06-14 13:35:21 +0100

    report worked on

--- Src/test_files/bug_files/found files/muladd-log2-3.jl
+++ Src/test_files/bug_files/found files/muladd-log2-3.jl
@@ -1,3 +1,14 @@
 x = muladd((5270536651468945146),(π),13656)
 println(x)
 println(x*(10^-19))
+println((5270536651468945146*π)+(13656))
+
+x = muladd((33//34),(3.0),(0.5938))
+println(x)
+println(33//34*3.0+0.5938)
+
+a=0
+for i in 1:9999999999999
+    global a+=1
+end
+println(a)
