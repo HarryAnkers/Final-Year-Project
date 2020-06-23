7e2da062849e24d4eab39a4eec94d999954f370e
Harrison Ankers <h-ankers@outlook.com>
2020-06-23 17:45:45 +0100

    presentation update

--- Src/test_files/bug_files/found files/muladd-log2-3.jl
+++ Src/test_files/bug_files/found files/muladd-log2-3.jl
@@ -6,9 +6,3 @@ println((5270536651468945146*π)+(13656))
 x = muladd((33//34),(3.0),(0.5938))
 println(x)
 println(33//34*3.0+0.5938)
-
-a=0
-for i in 1:9999999999999
-    global a+=1
-end
-println(a)
---
+++ Src/test_files/bug_files/test_13:52:04[2020-06-14].jl
@@ -0,0 +1,435 @@
+x = parse(Int64, ARGS[1])
+var_0 = ((291751247649244441425309100163070686895)==(true))
+var_1 = ((-86980433729359864634318697061023923968)^(0.15889273634280876))
+global var_0 = (-19)
+var_2 = (0.1934)
+var_3 = (true)
+if (false)
+	var_4 = (true)
+	for var_5 = 1:85
+		var_5 = (0.786)
+		var_4 = (true)
+		var_6 = var_1
+		var_7 = (!((25//37)>=(-39568201200696983520629583626122377062)))
+		var_4 = (12849 - 3739im)
+		var_8 = var_3
+		function func_0(var_9,)
+			var_10 = (var_6≤(16933189025709421564))
+			var_11 = (3)
+			return true
+		end
+	end
+	if ((false)⊻(false))
+		var_12 = (false)
+		var_13 = ceil((77),)
+		var_12 = ((~(68))>>>(false))
+		var_14 = var_13
+		if var_3
+		end
+		var_15 = (-43)
+		global var_0 = (0.3203)
+		global var_1 = (Float16(0.2686) + Float16(0.802)im)
+	end
+	var_16 = (6.0)
+	var_17 = (((71118863795370761846748409445779349937)>>>(true))&(-67))
+	if (true)
+	elseif (false)
+		if (((false)*(false))|(false))
+			var_18 = (true)
+			var_19 = ((-2232)⊻(-124))
+		end
+		var_20 = (12695016933625536945)
+		var_21 = (-152004505340842377883160608964472954151)
+		var_22 = (((-30783)>>(true))-(92697170922526553194796473475777007379))
+		var_23 = (0.14682852311929295)
+		var_20 = (Float16(0.4375) + Float16(0.0879)im)
+		for var_24 = 1:-64
+			var_25 = var_4
+			var_26 = (Float16(0.3535) + Float16(0.833)im)
+			var_27 = var_26
+		end
+		if (true)
+			var_22 = (25//54)
+		end
+		var_28 = var_3
+		if (false)
+			var_29 = (3178 + 25950im)
+		end
+		var_21 = (+(312701863429542901593311101545046461114))
+	end
+	global var_3 = (-110)
+	global var_3 = (0.3221407)
+	var_30 = (1203765067)
+else
+	for var_31 = x:-45
+	end
+	var_32 = (22500)
+	if (false)
+		var_33 = ((((-116231911196446097072960235296309841892)+((5858700094241779036)+(4403050927037467485)))>>(-81001865206348150295187792778176650966))>>>(7810831683654057599))
+	end
+	for var_34 = 1:89
+		var_35 = ((16//89)-(Float16(0.124) + Float16(0.08203)im))
+	end
+end
+for var_36 = 1:-14
+end
+if (false)
+	var_37 = (0.7042903)
+	if (true)
+		var_38 = (!(false))
+	end
+	var_39 = round((true),)
+else
+	var_40 = (false)
+	var_41 = (1.0)
+	function func_1(var_42,var_43,var_44,var_45,var_46,var_47,)
+		for var_48 = 1:-55
+			if (false)
+			end
+		end
+		var_49 = (Float16(0.4922) + Float16(0.957)im)
+		return true
+	end
+end
+if (false)
+	var_50 = (-381431962)
+	var_51 = (0.2657883213205048 + 0.0im)
+	if (true)
+		var_52 = ((2677122689)<(π))
+		for var_53 = x:-99
+			var_54 = (55191)
+			if (false)
+				var_50 = (1580400477)
+				var_55 = (3926377257911523411)
+				global var_0 = (0.5051696683428875 + 0.0im)
+				var_56 = (17//81)
+			end
+			var_57 = (64164)
+		end
+	end
+	var_58 = (62106159704596716086267976255736453125)
+	var_59 = (-27719)
+end
+for var_60 = x:55
+	if ((false)*(false))
+		var_61 = ((~(48))>(80506896154693941630014582080683782482))
+		var_60 = var_2
+	end
+	if (false)
+		var_62 = (π)
+		var_63 = (3)
+	else
+	end
+	global var_3 = var_0
+	var_60 = var_3
+	var_64 = ((298030470158801349274874852381072969513)&(118))
+	var_65 = (3)
+	global var_1 = var_1
+end
+global var_2 = (0.1834122)
+if (((1115840150708380763)>>>(6))<=(0.7956103))
+	global var_2 = (Float16(0.918) + Float16(0.9023)im)
+	var_66 = (64574)
+	var_67 = (66)
+	var_67 = (102)
+	if (var_67≤(6))
+		if (true)
+			var_67 = ((-3)>>var_67)
+			for var_68 = x:47
+			end
+			var_69 = (36//43)
+			var_70 = (false)
+			var_71 = ((100015581120338857407907717291276648322)⊻(-98681093768131644900962612611314286277))
+		end
+		var_72 = ((false)⊻(17810772722523636027))
+		var_73 = (2.0)
+		var_72 = (19//88)
+		var_66 = (-146303113161573961558486426719041968977)
+		var_74 = (9782517193951847599)
+	end
+	var_75 = (69)
+	for var_76 = x:-98
+		var_77 = (8711292799199064568)
+		for var_78 = x:-22
+		end
+		var_79 = (!(false))
+	end
+end
+var_80 = var_0
+var_81 = (0.23128068)
+var_82 = (4)
+var_83 = (-(1))
+if (false)
+end
+if (true)
+	var_84 = ((131379771165620718492309460994363082608)-(true))
+elseif (false)
+	var_85 = (41129947950219827067509745312724559678)
+	var_86 = (0.2910261203118778 + 0.0im)
+end
+var_87 = sinh((0.26348916593200156),)
+function func_2(var_88,)
+	if (false)
+	end
+	var_89 = ((false)|(9473170954907365392))
+	return true
+end
+if (((0.002750258326040589)>var_81)&(!(true)))
+	if (true)
+		function func_3(var_90,var_91,var_92,var_93,var_94,var_95,var_96,)
+			return true
+		end
+	end
+	global var_81 = (0.1789283141832505)
+	if (true)
+	end
+	function func_4()
+		return true
+	end
+	if ((0.5)<=(39//44))
+	end
+	var_97 = (!((true)*(true)))
+	var_97 = ((42078)<<(61))
+	var_98 = (0.26543725)
+	function func_5()
+		return true
+	end
+	var_99 = ((0.13925025675653502 + 0.0im)-var_2)
+	var_100 = ((134069739955767707647637233830795115379)&(!(true)))
+	for var_101 = 1:41
+	end
+end
+var_102 = (0.6044895436958173 + 0.0im)
+var_103 = var_0
+if (true)
+	var_104 = ((false)<<(true))
+	var_105 = (((0.4420011891403557 + 0.0im)+(-(0.6063994072298253 + 0.0im)))^(-var_2))
+	if (false)
+		for var_106 = x:-69
+		end
+		var_107 = (-3777983250053203887370939254866330875)
+		if (~(false))
+			if (true)
+				var_108 = func_2((false),)
+				var_107 = (Float16(0.5264) + Float16(0.461)im)
+			elseif (false)
+			else
+				if abs((false),)
+				elseif ((false)*(false))
+					var_109 = (1795477426)
+					var_110 = (0.5151668359474681 + 0.0im)
+					global var_81 = (-9452 - 31493im)
+					var_111 = (-1688092334)
+				elseif (false)
+				end
+				var_112 = (π)
+			end
+		end
+		if ceil((true),)
+			var_113 = (609140013)
+			var_114 = (-292962379)
+			var_115 = (0.7899737601211623)
+			global var_82 = (0.15594971)
+		end
+		var_104 = ((false)|((false)&ceil((86),)))
+	else
+		return (true)
+		return (376576100)
+		global var_82 = ((131401166135260916224295066343200622465)^((0.6152428883064169)+(-91816809365001488456715831716591644884)))
+	end
+	var_116 = (false)
+	var_117 = (5.0)
+	return ceil((false),)
+	var_116 = (22494)
+	function func_6(var_118,var_119,var_120,var_121,var_122,)
+		return true
+	end
+	var_123 = (3.0)
+	if (((false)&(false))*(true))
+	elseif (false)
+		var_124 = (0.19786227241520415)
+		var_125 = (false)
+	elseif ((false)&(((true)&(false))|((16051205260367696284)>(0.29698511568542596))))
+		var_126 = (59634868661739403583996795725353569370)
+		function func_7(var_127,var_128,)
+			for var_129 = x:-45
+				for var_130 = 1:0
+					var_131 = (3.0)
+					var_129 = (75432691501425431623979277684138764569)
+					var_132 = (false)
+				end
+			end
+			var_133 = var_0
+			return true
+		end
+		var_134 = (9957)
+		var_123 = cosc(((π)==(-3860747336098846439)),)
+	end
+	for var_135 = x:-65
+		if (true)
+			var_136 = (0.20036878480244802)
+			var_137 = func_2((3296003348),)
+			var_138 = (var_135<(6))
+		end
+	end
+	var_139 = (true)
+	function func_8(var_140,)
+		var_141 = (4867736649213784625)
+		return true
+	end
+	if (~(false))
+	end
+elseif (true)
+end
+return (585500459)
+for var_142 = 1:-17
+	global var_82 = (Float16(0.745) + Float16(0.5244)im)
+	var_143 = (Float16(0.909) + Float16(0.4717)im)
+	for var_144 = 1:-3
+		var_144 = ((!(true))<<(1759780400))
+		for var_145 = x:-74
+			var_146 = (3.0)
+			var_147 = (-1722785159)
+			var_148 = (38278)
+			var_147 = (-82311958823913777150244984026354551887)
+			var_149 = (-7257503159121742927)
+			var_144 = (2879637370358699703)
+			function func_9(var_150,var_151,var_152,var_153,)
+				return true
+			end
+		end
+		var_154 = func_2((-177286767465067319),)
+		var_155 = (true)
+	end
+	for var_156 = 1:22
+		function func_10()
+			return (4)
+			return true
+		end
+	end
+	var_157 = (720679197)
+	function func_11(var_158,)
+		return true
+	end
+	var_159 = (0.2373)
+	var_160 = (-88783640472806068)
+	for var_161 = x:10
+		var_162 = (5.0)
+		var_161 = ((1439281674)>>(false))
+		var_162 = (0.6107330015296455 + 0.0im)
+		if (true)
+			if (((((7213867134593379050)>=(0.91))≥(π))⊻(false))&(true))
+			elseif (false)
+				var_157 = (17//76)
+				for var_163 = 1:40
+				end
+				var_164 = ((-138118455514274901770571423088627259379)|(6))
+			end
+		elseif (true)
+			var_160 = ((191)>>>((-3504721894950379473)-((35862)>>>(false))))
+			var_165 = (52790)
+		end
+		if (true)
+		elseif (true)
+			for var_166 = x:-3
+				var_167 = (-148156845236850294975245012545095239191)
+				if ((false)≥(48639929847977026722335732947502976218))
+				elseif ((true)|(false))
+					var_166 = (8727835942734744794)
+					var_168 = (-27154)
+					if (true)
+					end
+					if (func_11(((2052361894)==((50)>(-106))),)==var_166)
+					end
+					var_167 = (-(0.3428))
+				end
+			end
+			if (false)
+				for var_169 = x:62
+				end
+			elseif (false)
+			else
+				if (false)
+				elseif ceil((((false)>=(π))*((2613534385952238654)≤((39217088364887738977243196274242430175)≤(102)))),)
+					for var_170 = x:-48
+						var_171 = (+(0.3395139741365891 + 0.0im))
+						var_170 = (-1036244297)
+					end
+				end
+				var_172 = (false)
+				var_172 = (69525893386825524670302257943804683850)
+				var_173 = (0.3414117840357358 + 0.0im)
+				var_174 = (40//119)
+				var_175 = (41638)
+				global var_83 = (~((9421235512086931018)+func_2(((220)<<((4059)⊻(4))),)))
+				if (true)
+				end
+				var_176 = (4304011138358514164782995590442178613)
+				var_177 = (1247144730)
+				function func_12(var_178,var_179,var_180,var_181,var_182,var_183,)
+					return true
+				end
+			end
+		end
+		var_184 = ((!(false))+(-292654088))
+		var_185 = (0.9179779)
+		var_186 = (4224107666)
+	end
+	var_187 = (0.9106749300994015 + 0.0im)
+	global var_1 = var_143
+	var_188 = (3.0)
+	var_189 = (true)
+	if (false)
+		function func_13(var_190,var_191,var_192,var_193,)
+			return true
+		end
+		if (false)
+		end
+	elseif (false)
+	end
+	if (true)
+		var_194 = (45535)
+	elseif ((true)*var_189)
+	end
+	var_195 = ((4.0)>=(0.22077608))
+	var_196 = (43507)
+	function func_14(var_197,var_198,)
+		return true
+	end
+	for var_199 = x:74
+		var_189 = ((3.0)≥(false))
+	end
+end
+if (false)
+	var_200 = (2)
+elseif (true)
+else
+	var_201 = (0.33)
+	var_202 = (false)
+	if (true)
+	elseif (true)
+		var_203 = (15//58)
+	end
+	var_204 = var_80
+	var_202 = (false)
+	for var_205 = x:0
+	end
+	var_206 = (5143134300740545007)
+	return (-123)
+	function func_15(var_207,var_208,var_209,)
+		return true
+	end
+	var_210 = (295074262977290109220928536231191771515)
+end
+if (true)
+	var_211 = (0.19894645776567832)
+end
+var_212 = ((7750829233530993019)^(7))
+for var_213 = x:18
+end
+var_214 = (387068381)
+var_215 = ((π)+(true))
+
+tmpLog = open(string("/Users/harrisonankers/Documents/UNI/Year 4/FYP/Src/test_files/Process_0/log_files/log",(ARGS[2]),".txt"), "w")
+write(tmpLog, string(var_0,",",var_1,",",var_2,",",var_3,",",var_80,",",var_81,",",var_82,",",var_83,",",var_87,",",var_102,",",var_103,",",var_212,",",var_214,",",var_215,",",0,"\n"))
+close(tmpLog)
